13306f86faed57150dd4b4d4c45727c3ed7274fd
Chris Wilcox <ckwilcox@gmail.com>
2013-01-18 18:03:42 -0800

    Realhand trackign started

--- client.py
+++ client.py
@@ -1,6 +1,69 @@
 import Leap, sys
 import OSC
 from OSC import OSCClient, OSCMessage
+from collections import defaultdict
+from datetime import datetime
+from itertools import count
+
+
+{
+    'last_seen'
+
+}
+
+
+class RealHandTracker(object):
+
+    def __init__(self, hand_miss_count=5):
+        self._real_hands = {}
+        self._by_id = {}
+        self.hand_miss_count = hand_miss_count
+        self.frame_count = 0
+
+    def get_real_number(self, hand):
+        return self._by_id.get(hand.id)
+
+    def frame_tick(self, frame):
+        self.frame_count += 1
+
+        for real_num, hand_data in self._real_hands.items():
+            last_seen_frame, real_hand = hand_data
+            if self.frame_count - last_seen_frame >= self.hand_miss_count:
+                # This real hand data is old! Purge it!
+                del self._real_hands[real_num]
+                del self._by_id[real_hand.id]
+
+        for hand in frame.hands:
+            self.handle(hand)
+
+
+    def handle(self, hand):
+        real_num = self.get_real_number(hand)
+        if real_num:
+            # We found the real number for this hand, we're done
+            return 
+
+        real_num = self.get_next_real_number()
+        self._by_id[hand.id] = real_num
+        self._real_hands[real_num] = (self.frame_count, hand)
+
+    def get_next_real_number(self):
+        for i in count(1):
+            if self._real_hands.get(i):
+                return i
+
+    @property
+    def hands(self):
+        real_hand_count = len(self._real_hands)
+        r_counter = 0
+        for i in count(1):
+            if r_counter == real_hand_count:
+                raise StopIteration
+            r = self._real_hands.get(i)
+            if r:
+                r_counter += 1
+                yield r
+
 
 
 class OSCLeapListener(Leap.Listener):
@@ -8,13 +71,19 @@ class OSCLeapListener(Leap.Listener):
     DEBUG = True
 
     def __init__(self, *args, **kwargs):
+        self.frame_count = 0
+        self.saw_finger = False
+        # Settings
         self.client = kwargs.pop('client', OSCClient())
         self.hostname = kwargs.pop('hostname','localhost')
         self.port = kwargs.pop('port',7110)
+        self.dumb = kwargs.pop('dumb',False)
+        # OSC Connect
         print "Connecting to OSC server at '%s:%s'" % (self.hostname,self.port)
-        self.client.connect( (self.hostname, self.port) )
-        self.frame_count = 0
-        self.saw_finger = False
+        self.client.connect((self.hostname, self.port))
+
+        self.real_hand_tracker = RealHandTracker()
+
         super(OSCLeapListener,self).__init__(*args,**kwargs)
 
     def on_init(self, controller):
@@ -46,9 +115,42 @@ class OSCLeapListener(Leap.Listener):
         self.send("%sy" % base, vector[1])
         self.send("%sz" % base, vector[2])
 
-    def on_frame(self, controller):
+    def _on_frame_smart(self, controller):
         frame = controller.frame()
+
+        self.real_hand_tracker.frame_tick(frame)
+        for hand in self.real_hand_tracker.hands:
+
+
+
+
+            hand_dict = hand_db.get(hand.id):
+            if hand_dict:
+
+            else:
+                hand_db[hand.id] = {
+                    ""
+                }
+
+
+            else:
+                # New hand!
+                ["ts"] = datetime.now()
+
+
+            hand_base = "/hand%d" % hand.id
+
+
+
+    def on_frame(self, controller):
         self.frame_count += 1
+        if self.dumb:
+            return self._on_frame_dumb(controller)
+        else:
+            return self._on_frame_smart(controller)
+
+    def _on_frame_dumb(self, controller):
+        frame = controller.frame()
         #print "Frame id: %d, timestamp: %d, hands: %d, fingers: %d, tools: %d" % (
         #      frame.id, frame.timestamp, len(frame.hands), len(frame.fingers), len(frame.tools))
 
